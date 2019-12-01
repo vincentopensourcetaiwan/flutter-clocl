1c755cd4f5dabf373b03d65c079478e0377990a3
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2019-12-01 10:51:37 +0800

    Flutter Clock

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'dart:ui' as ui;
 import 'dart:ui' as ui;
 
 void main() => runApp(MyApp());
@@ -7,10 +10,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: HomeWidget(),
+      title: 'Flutter Time Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(title: 'Flutter Time Demo'),
     );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _timeString;
+
+  @override
+  void initState() {
+    _timeString = _formatDateTime(DateTime.now());
+    Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: HomeWidget(),
+    );
+  }
+
+  void _getTime() {
+    final DateTime now = DateTime.now();
+    final String formattedDateTime = _formatDateTime(now);
+    setState(() {
+      _timeString = formattedDateTime;
+    });
+  }
+
+  String _formatDateTime(DateTime dateTime) {
+    return DateFormat('MM/dd/yyyy hh:mm:ss').format(dateTime);
   }
 }
 
@@ -18,7 +65,8 @@ class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: CustomPaint( //                       <-- CustomPaint widget
+      child: CustomPaint(
+        //                       <-- CustomPaint widget
         size: Size(300, 300),
         painter: MyPainter(),
       ),
@@ -26,19 +74,20 @@ class HomeWidget extends StatelessWidget {
   }
 }
 
-class MyPainter extends CustomPainter { //         <-- CustomPainter class
+class MyPainter extends CustomPainter {
+  //         <-- CustomPainter class
+  var now = new DateTime.now();
+
   @override
   void paint(Canvas canvas, Size size) {
     final textStyle = ui.TextStyle(
       color: Colors.black,
       fontSize: 30,
     );
-    final paragraphStyle = ui.ParagraphStyle(
-      textDirection: TextDirection.ltr,
-    );
+    final paragraphStyle = ui.ParagraphStyle();
     final paragraphBuilder = ui.ParagraphBuilder(paragraphStyle)
       ..pushStyle(textStyle)
-      ..addText('Hello, world.');
+      ..addText(DateFormat('MM/dd/yyyy hh:mm:ss').format(now));
     final constraints = ui.ParagraphConstraints(width: 300);
     final paragraph = paragraphBuilder.build();
     paragraph.layout(constraints);
@@ -48,6 +97,6 @@ class MyPainter extends CustomPainter { //         <-- CustomPainter class
 
   @override
   bool shouldRepaint(CustomPainter old) {
-    return false;
+    return true;
   }
 }
